473fe4774cd239381aeba6a4d67a146af8d56113
pranjal <pranjalkhatri950@gmail.com>
2025-10-02 08:19:05 +0000

    added signed types

--- kernel/types.h
+++ kernel/types.h
@@ -1,10 +1,17 @@
-typedef unsigned int   uint;
-typedef unsigned short ushort;
-typedef unsigned char  uchar;
+typedef unsigned int    uint;
+typedef unsigned short  ushort;
+typedef unsigned char   uchar;
 
-typedef unsigned char uint8;
-typedef unsigned short uint16;
-typedef unsigned int  uint32;
-typedef unsigned long uint64;
+typedef char            int8;
+typedef unsigned char   uint8;
+
+typedef short           int16;
+typedef unsigned short  uint16;
+
+typedef int             int32;
+typedef unsigned int    uint32;
+
+typedef long            int64;
+typedef unsigned long   uint64;
 
 typedef uint64 pde_t;
