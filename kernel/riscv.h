296a45c7313e7ea525714a42a795b5e549d077ea
pranjal <pranjalkhatri950@gmail.com>
2025-10-05 07:30:59 +0000

    flag defined for cow fork

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -357,12 +357,13 @@ typedef uint64 *pagetable_t; // 512 PTEs
 #define PGROUNDUP(sz, pgsz) (((sz) + pgsz - 1) & ~(pgsz - 1))
 #define PGROUNDDOWN(a, pgsz) (((a)) & ~(pgsz - 1))
 
-#define PTE_V (1L << 0) // valid
-#define PTE_R (1L << 1)
-#define PTE_W (1L << 2)
-#define PTE_X (1L << 3)
-#define PTE_U (1L << 4) // user can access
-#define PTE_S (1L << 8) // need to be swapped in
+#define PTE_V   (1L << 0) // valid
+#define PTE_R   (1L << 1)
+#define PTE_W   (1L << 2)
+#define PTE_X   (1L << 3)
+#define PTE_U   (1L << 4) // user can access
+#define PTE_S   (1L << 8) // need to be swapped in
+#define PTE_COW (1L << 9) // cow page, copy and write on page fault
 
 // shift a physical address to the right place for a PTE.
 #define PA2PTE(pa) ((((uint64)pa) >> 12) << 10)
