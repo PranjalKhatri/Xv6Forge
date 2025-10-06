14d9730e21cabd70ead030a9c7470160f17beed0
pranjal <pranjalkhatri950@gmail.com>
2025-10-06 18:09:50 +0000

    MAXVA check first on page fault

--- kernel/trap.c
+++ kernel/trap.c
@@ -69,7 +69,10 @@ usertrap(void)
   } else if((which_dev = devintr()) != 0){
     // ok
   } else if((r_scause() == 0xf || r_scause() == 0xd || r_scause() == 0xc) ) {
-    if(vmfault(p->pagetable, r_stval(), (r_scause() == 13)? 1 : 0,r_scause() == 0xc) != 0){
+    if(r_stval() >= MAXVA || r_stval() >= p->sz){
+      setkilled(p);
+    }
+    else if(vmfault(p->pagetable, r_stval(), (r_scause() == 13)? 1 : 0,r_scause() == 0xc) != 0){
       // page fault on lazily-allocated page
     }else{
       setkilled(p);
