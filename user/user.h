71fd84aaa3e1e898bd2b53ee747ab4f42326aed5
pranjal <pranjalkhatri950@gmail.com>
2025-10-21 17:54:18 +0000

    added unix getline

--- user/user.h
+++ user/user.h
@@ -53,6 +53,7 @@ int memcmp(const void *, const void *, uint);
 void *memcpy(void *, const void *, uint);
 char* sbrk(int);
 char* sbrklazy(int);
+int getline(char **buf, int *len, int fp);
 
 // printf.c
 void fprintf(int, const char*, ...) __attribute__ ((format (printf, 2, 3)));
